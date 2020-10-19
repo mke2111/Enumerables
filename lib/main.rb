b64f237e1fe19bbce6413f5adb36c80eef9f45ff
mke2111 <codejose5@gmail.com>
2020-10-19 22:37:39 +0300

    Added my_each_with_index method

--- lib/main.rb
+++ lib/main.rb
@@ -15,4 +15,18 @@ module Enumerable
     items
   end
 
+  # ------------my_each_with_index----------
+
+  def my_each_with_index
+    to_enum(:my_each_with_index) unless block_given?
+
+    items = to_a
+    count = 0
+    until count > items.length - 1
+      yield(items[count], count)
+      count += 1
+    end
+    items
+  end
+
 end
